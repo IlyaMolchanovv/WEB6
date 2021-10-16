f80225ad4975b4128d4861bf34062afb4ffc2105
IlyaMolchanovv <90561936+IlyaMolchanovv@users.noreply.github.com>
2021-10-16 09:46:29 +0300

    Update script.js

--- script.js
+++ script.js
@@ -51,7 +51,6 @@ function getPrices() {
   return {
     prodTypes: [300, 50000, 70000],
     prodOptions: {
-      option1: 500
       option2: 1000,
       option3: 2000,
     },
@@ -105,8 +104,7 @@ window.addEventListener('DOMContentLoaded', function (event) {
       console.log(d.value);
 	  console.log(d.name);
       updatePrice();
-    });
+});
   });
-
-  updatePrice();
+updatePrice();
 });
